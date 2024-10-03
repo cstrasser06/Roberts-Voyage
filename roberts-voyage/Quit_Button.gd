bdaeee401cc4b532780508d357c980d1ed2c138b
StarkJohannes <johannes@stark-net.at>
2024-10-03 16:31:41 +0200

    Main-Menu
    Logik Fertig; Design fehlt

--- roberts-voyage/Quit_Button.gd
+++ roberts-voyage/Quit_Button.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func _pressed() -> void:
-	get_tree().change_scene_to_file("res://Startroom.tscn");
+	get_tree().quit();
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
