4c85f85a779969e47ba5ceac2c6e2461027013e3
Noblance <schulnerjonas@gmail.com>
2024-10-10 14:36:18 +0200

    Added Player Stats and global player

--- roberts-voyage/Player.gd
+++ roberts-voyage/Player.gd
@@ -1,11 +1,11 @@
 extends Node
 
+var currentHP = 3
+var maxHP = 3
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var currentEnergy = 3
+var maxEnergy = 3
 
+var Items = {}
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var Ability = ""
---
+++ roberts-voyage/character_body_2d.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var directionx := Input.get_axis("MOVELEFT", "MOVERIGHT")
+	var directiony := Input.get_axis("MOVEUP", "MOVEDOWN")
+	if directionx || directiony:
+		velocity.x = directionx * SPEED
+		velocity.y = directiony * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
