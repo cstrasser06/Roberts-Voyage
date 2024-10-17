52ca4dc6cdfbebff19a8318f544fedfdd7e46d8b
Noblance <schulnerjonas@gmail.com>
2024-10-17 16:29:34 +0200

    Room Architecture and Creation of Maps

--- roberts-voyage/Scripts/RoomGeneration.gd
+++ roberts-voyage/Scripts/RoomGeneration.gd
@@ -6,7 +6,15 @@ var Bioms = ["U", "S", "H"]
 
 var currentLevelinBiom = 0
 
-#Old Room is a parameter to delete from the screen
-func createnewroom(oldroom:Node2D):
-	oldroom.queue_free();
+
+
+
+func createnewroom():
+	var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+	if(currentLevelinBiom == 3):
+		get_tree().change_scene_to_file("res://Scenes/Rooms/Room_"+ currentBiom + characters[randi_range(0,0)]+"1.tscn")
+	else:
+		get_tree().change_scene_to_file("res://Scenes/Rooms/Room_"+ currentBiom + characters[randi_range(0,0)]+"0.tscn")
 	
+func createnewbossroom():
+	pass
--- roberts-voyage/Scripts/exit.gd
+++ roberts-voyage/Scripts/exit.gd
@@ -9,5 +9,5 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	print("Bye")
 	body.queue_free();
-	get_tree().change_scene_to_file("res://Scenes/Main_Menu.tscn");
+	RoomGeneration.createnewroom();
 	
