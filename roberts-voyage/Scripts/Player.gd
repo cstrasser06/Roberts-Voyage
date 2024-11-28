95a35c510b98e35ec7af2e46005e4a88d8cea39f
Noblance <schulnerjonas@gmail.com>
2024-11-28 15:11:46 +0100

    Player reset

--- roberts-voyage/Scripts/Player.gd
+++ roberts-voyage/Scripts/Player.gd
@@ -9,3 +9,8 @@ var maxEnergy = 3
 var Items = {}
 
 var Ability = ""
+
+
+func _process(delta: float) -> void:
+	if currentHP <= 0:
+		get_tree().change_scene_to_file("res://Scenes/Main_Menu.tscn");
