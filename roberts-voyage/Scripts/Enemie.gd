59f267af78af6111361091869c8673c12dc65aba
StarkJohannes <johannes@stark-net.at>
2024-11-28 14:23:51 +0100

    Enemie geht wida

--- roberts-voyage/Scripts/Enemie.gd
+++ roberts-voyage/Scripts/Enemie.gd
@@ -3,9 +3,9 @@ extends Node2D
 
 #ENEMIE MELEE ATTACK ------------------------------------------
 
-func _on_melee_hit_body_entered(body: Node2D) -> void:
-	if body.name == "PlayerCharacter":
-		Player.currentHP -= 1
+#func _on_melee_hit_body_entered(body: Node2D) -> void:
+#	if body.name == "PlayerCharacter":
+#		Player.currentHP -= 1
 
 
 
@@ -29,7 +29,7 @@ var velocity: Vector2 = Vector2.ZERO
 var change_direction_time: float = 2.0
 var time_passed: float = 0.0
 var wait_time: float = 0.0
-var wait_time_direction = 0.0
+var wait_time_direction: float = 0.0
 
 func _ready():
 	randomize()
@@ -40,7 +40,7 @@ func _physics_process(delta):
 	wait_time_direction = randf_range(1.0, 3.0)
 	time_passed += delta
 	if time_passed >= change_direction_time:
-		velocity = Vector2(0,0)
+		#velocity = Vector2(0,0)
 		wait_time += delta
 		if wait_time >= wait_time_direction:
 			velocity = get_random_direction() * speed
